ffe3ff46afd67cc006f6b9b78c53fd562b5618d2
Kevin <kmembreno@unah.hn>
2022-11-17 17:24:50 -0600

    feat - add validate inputs

--- Main.cpp
+++ Main.cpp
@@ -65,6 +65,8 @@ void hacerAumentos();
 void menuSalarios();
 void hacerBono();
 bool findEmp(int id);
+bool validId(int id);
+int validOp(int opcion);
 
 int arr[2];
 const int fila = 5, columna = 8;
@@ -92,8 +94,10 @@ int main()
     }
   }
 
-  menuDeducciones();
+  // menuDeducciones();
   // menuSalarios();
+  // menuPlanilla();
+  menuPrincpipal();
 
   // MOSTRAR
   // for (int i = 0; i < fila; i++)
@@ -102,140 +106,134 @@ int main()
   return 0;
 }
 
-// void menuPrincpipal()
-// {
-//   bool state = true;
-//   int opcion;
-//   do
-//   {
-//     cout << "MENU PRINCIPAL\n";
-//     cout << "1. ver planilla / 2. ver deducciones  / 3. realizar aumento / 4. ver salario sin deducciones / 5. bonos aleatorios / 6. limpiar pantalla / 7. salir\n";
-//     cout << "Ingrese el numero de la opcion: ";
-//     cin >> opcion;
+void menuPrincpipal()
+{
+  bool state = true;
+  int opcion;
+  do
+  {
+    system("cls");
+    cout << "MENU PRINCIPAL\n";
+    cout << "1. ver planilla / 2. ver deducciones  / 3. realizar aumento / 4. ver salario sin deducciones / 5. bonos aleatorios / 6. salir\n";
+    cout << "Ingrese el numero de la opcion: ";
+    cin >> opcion;
 
-//     switch (opcion)
-//     {
-//     case 1:
-//       menuPlanilla();
-//       break;
-//     case 2:
-//       menuDeducciones();
-//       break;
-//     case 3:
-//       hacerAumentos();
-//       break;
-//     case 4:
-//       menuSalarios();
-//       break;
-//     case 5:
-//       hacerBono();
-//       break;
-//     case 6:
-//       system("cls");
-//       break;
-//     case 7:
-//       state = false;
-//       break;
-//     default:
-//       cout << "Ingres una opcion valida\n";
-//     }
-//   } while (state);
-// }
+    switch (validOp(opcion))
+    {
+    case 1:
+      menuPlanilla();
+      break;
+    case 2:
 
-// void menuPlanilla()
-// {
-//   bool state = true;
-//   int opcion;
-//   do
-//   {
-//     cout << "MENU DE PLANILLAS\n";
-//     cout << "1. mostrar planilla individual / 2. mostrar planilla general / 3. limpiar pantalla / 4. regresar al menu principal\n";
-//     cin >> opcion;
-//     switch (opcion)
-//     {
-//     case 1:
-//       int id;
-//       cout << "ingrese el id del empleado: ";
-//       cin >> id;
+      menuDeducciones();
+      break;
+    case 3:
 
-//       if (findEmp(id))
-//       {
-//         cout << setw(8) << left << "ID"
-//              << setw(20) << left << "CARGO"
-//              << setw(18) << left << "SALARIO"
-//              << setw(16) << left << "QUINCENA"
-//              << setw(18) << left << "DEDUCCIONES"
-//              << setw(16) << left << "BONO"
-//              << setw(16) << left << "TOTAL"
-//              << endl;
-//         cout << "-------------------------------------------------------------------------------------------------------------------------------------" << endl;
-//         cout << setw(8) << left << employees[arr[0]][arr[1]].getId()
-//              << setw(20) << left << employees[arr[0]][arr[1]].getCargo()
-//              << setw(18) << left << employees[arr[0]][arr[1]].getSalario()
-//              << setw(16) << left << employees[arr[0]][arr[1]].getQuincena()
-//              << setw(28) << left << employees[arr[0]][arr[1]].getDeducciones()
-//              << setw(26) << left << employees[arr[0]][arr[1]].getBono()
-//              << setw(26) << left << employees[arr[0]][arr[1]].getQuincenaDeducida() + employees[arr[0]][arr[1]].getBono()
-//              << endl;
-//       }
-//       else
-//       {
-//         cout << "empleado no encontrado\n";
-//       }
-//       break;
-//     case 2:
-//       cout << setw(8) << left << "ID"
-//            << setw(20) << left << "CARGO"
-//            << setw(18) << left << "SALARIO"
-//            << setw(16) << left << "QUINCENA"
-//            << setw(18) << left << "DEDUCCIONES"
-//            << setw(16) << left << "BONO"
-//            << setw(16) << left << "TOTAL"
-//            << endl;
-//       cout << "-------------------------------------------------------------------------------------------------------------------------------------" << endl;
-//       for (int i = 0; i < fila; i++)
-//       {
-//         if (i < 2)
-//         {
-//           for (int j = 0; j < columna - 4; j++)
-//           {
-//             cout << setw(8) << left << employees[i][j].getId()
-//                  << setw(20) << left << employees[i][j].getCargo()
-//                  << setw(18) << left << employees[i][j].getSalario()
-//                  << setw(16) << left << employees[i][j].getQuincena()
-//                  << setw(28) << left << employees[i][j].getDeducciones()
-//                  << setw(26) << left << employees[i][j].getBono()
-//                  << setw(26) << left << employees[i][j].getQuincenaDeducida() + employees[i][j].getBono()
-//                  << endl;
-//           }
-//         }
-//         else
-//         {
-//           for (int j = 0; j < columna; j++)
-//           {
-//             cout << setw(8) << left << employees[i][j].getId()
-//                  << setw(20) << left << employees[i][j].getCargo()
-//                  << setw(18) << left << employees[i][j].getSalario()
-//                  << setw(16) << left << employees[i][j].getQuincena()
-//                  << setw(28) << left << employees[i][j].getDeducciones()
-//                  << setw(26) << left << employees[i][j].getBono()
-//                  << setw(26) << left << employees[i][j].getQuincenaDeducida() + employees[i][j].getBono()
-//                  << endl;
-//           }
-//         }
-//       }
-//       break;
-//     case 3:
-//       system("cls");
-//       break;
-//     case 4:
-//       state = false;
-//       break;
-//     default:
-//       cout << "Ingres una opcion valida\n";
-//     }
-//   } while (state);
-// }
+      hacerAumentos();
+      break;
+    case 4:
+
+      menuSalarios();
+      break;
+    case 5:
+
+      hacerBono();
+      break;
+    case 6:
+      state = false;
+      break;
+    default:
+      cout << "ERROR: valor invalido\n";
+      system("pause>nul");
+      break;
+    }
+  } while (state);
+}
+
+void menuPlanilla()
+{
+  bool state = true;
+  int opcion;
+  do
+  {
+    system("cls");
+    cout << "MENU DE PLANILLAS\n";
+    cout << "1. mostrar planilla individual / 2. mostrar planilla general / 3. regresar al menu principal\n";
+    cout << "ingrese el numero de la opcion: ";
+    cin >> opcion;
+    switch (validOp(opcion))
+    {
+    case 1:
+      int id;
+      cout << "ingrese el id del empleado: ";
+      cin >> id;
+      if (findEmp(id) || validId(id))
+      {
+        cout << "-------------------------------------------------------------------------------------------------------" << endl;
+
+        cout << setw(8) << left << "ID"
+             << setw(20) << left << "CARGO"
+             << setw(18) << left << "SALARIO"
+             << setw(16) << left << "QUINCENA"
+             << setw(18) << left << "DEDUCCIONES"
+             << setw(16) << left << "BONO"
+             << setw(16) << left << "TOTAL"
+             << endl;
+        cout << "-------------------------------------------------------------------------------------------------------" << endl;
+
+        cout << setw(8) << left << employees[arr[0]][arr[1]].getId()
+             << setw(20) << left << employees[arr[0]][arr[1]].getCargo()
+             << setw(18) << left << employees[arr[0]][arr[1]].getSalario()
+             << setw(16) << left << employees[arr[0]][arr[1]].getQuincena()
+             << setw(18) << left << employees[arr[0]][arr[1]].getDeducciones()
+             << setw(16) << left << employees[arr[0]][arr[1]].getBono()
+             << setw(16) << left << employees[arr[0]][arr[1]].getQuincenaDeducida() + employees[arr[0]][arr[1]].getBono()
+             << endl;
+        cout << "-------------------------------------------------------------------------------------------------------" << endl;
+        system("pause>nul");
+      }
+      else
+      {
+        cout << "empleado no encontrado\n";
+      }
+      break;
+    case 2:
+      cout << "-------------------------------------------------------------------------------------------------------" << endl;
+      cout << setw(8) << left << "ID"
+           << setw(20) << left << "CARGO"
+           << setw(18) << left << "SALARIO"
+           << setw(16) << left << "QUINCENA"
+           << setw(18) << left << "DEDUCCIONES"
+           << setw(16) << left << "BONO"
+           << setw(16) << left << "TOTAL"
+           << endl;
+      cout << "-------------------------------------------------------------------------------------------------------" << endl;
+      for (int i = 0; i < fila; i++)
+      {
+        for (int j = 0; j < columna; j++)
+        {
+          cout << setw(8) << left << employees[i][j].getId()
+               << setw(20) << left << employees[i][j].getCargo()
+               << setw(18) << left << employees[i][j].getSalario()
+               << setw(16) << left << employees[i][j].getQuincena()
+               << setw(18) << left << employees[i][j].getDeducciones()
+               << setw(16) << left << employees[i][j].getBono()
+               << setw(16) << left << employees[i][j].getQuincenaDeducida() + employees[i][j].getBono()
+               << endl;
+          cout << "-------------------------------------------------------------------------------------------------------" << endl;
+        }
+      }
+      system("pause>nul");
+      break;
+    case 3:
+      state = false;
+      break;
+    default:
+      cout << "ERROR: valor invalido\n";
+      system("pause>nul");
+    }
+  } while (state);
+}
 
 void menuDeducciones()
 {
@@ -243,17 +241,19 @@ void menuDeducciones()
   int opcion;
   do
   {
-    cout << "1. mostrar deducciones individuales / 2. mostrar deducciones generales / 3. limpiar pantalla / 4. volver al menu principal\n";
+    system("cls");
+    cout << "MENU DEDUCCIONES\n";
+    cout << "1. mostrar deducciones individuales / 2. mostrar deducciones generales / 3. volver al menu principal\n";
     cout << "Ingrese el numero de la opcion: ";
     cin >> opcion;
-    switch (opcion)
+    switch (validOp(opcion))
     {
     case 1:
       int id;
       cout << "ingrese el id del empleado: ";
       cin >> id;
 
-      if (findEmp(id))
+      if (findEmp(id) || validId(id))
       {
         cout << endl;
         cout << "-------------------------------------------------------------------------------------------------------------------------------------" << endl;
@@ -277,10 +277,12 @@ void menuDeducciones()
              << setw(16) << left << employees[arr[0]][arr[1]].getCooperativa()
              << endl;
         cout << "-------------------------------------------------------------------------------------------------------------------------------------" << endl;
+        system("pause>nul");
       }
       else
       {
         cout << "empleado no encontrado\n";
+        system("pause>nul");
       }
       break;
     case 2:
@@ -312,43 +314,49 @@ void menuDeducciones()
           cout << "-------------------------------------------------------------------------------------------------------------------------------------" << endl;
         }
       }
+      system("pause>nul");
       break;
     case 3:
-      system("cls");
-      break;
-    case 4:
       state = false;
       break;
     default:
-      cout << endl;
-      cout << "Ingres una opcion valida\n";
+      cout << "ERROR: valor invalido\n";
+      system("pause>nul");
     }
   } while (state);
 }
 
 void hacerAumentos()
 {
+
   int id;
   float aumento;
 
   cout << "ingrese el id del empleado: ";
   cin >> id;
 
-  if (findEmp(id))
+  if (findEmp(id) || validId(id))
   {
-    do
+    cout << "ingrese la cantidad del aumento: ";
+    cin >> aumento;
+    while (cin.fail() || aumento < 0)
     {
-      cout << "ingrese la cantidad del aumento: ";
+
+      cout << "\nIngrese un valor valido\n";
+      cin.clear();
+      cin.ignore(256, '\n');
+      cout << "Ingrese la cantidad del aumento: ";
       cin >> aumento;
-      if (aumento < 0)
-        cout << "ingrese un valor valido\n";
-    } while (aumento < 0);
+    }
+
     employees[arr[0]][arr[1]].setAumento(aumento);
     cout << "salario actualizado: " << employees[arr[0]][arr[1]].getSalario() << endl;
+    system("pause>nul");
   }
   else
   {
     cout << "empleado no encontrado\n";
+    system("pause>nul");
   }
 }
 
@@ -358,18 +366,18 @@ void menuSalarios()
   int opcion;
   do
   {
+    system("cls");
     cout << "MENU SALARIOS\n";
-    cout << "1. mostrar salario individual / 2. mostrar salarios de empleados / 3. limpiar pantalla / 4. volver al menu principal\n";
+    cout << "1. mostrar salario individual / 2. mostrar salarios de empleados / 3. volver al menu principal\n";
     cout << "Ingrese el numero de la opcion: ";
     cin >> opcion;
-    switch (opcion)
+    switch (validOp(opcion))
     {
     case 1:
       int id;
       cout << "ingrese el id del empleado: ";
       cin >> id;
-
-      if (findEmp(id))
+      if (findEmp(id) || validId(id))
       {
         cout << endl;
         cout << "---------------------------------------------------------" << endl;
@@ -384,10 +392,12 @@ void menuSalarios()
              << endl;
         cout << "---------------------------------------------------------" << endl;
         cout << endl;
+        system("pause>nul");
       }
       else
       {
         cout << "empleado no encontrado\n";
+        system("pause>nul");
       }
       break;
     case 2:
@@ -427,16 +437,14 @@ void menuSalarios()
                << employees[i][j].getQuincena() << endl;
           cout << "---------------------------------------------------------" << endl;
         }
-
+      system("pause>nul");
       break;
     case 3:
-      system("cls");
-      break;
-    case 4:
       state = false;
       break;
     default:
-      cout << "ingrese una opcion valida\n";
+      cout << "ERROR: valor invalido\n";
+      system("pause>nul");
     }
   } while (state);
 }
@@ -446,18 +454,22 @@ void hacerBono()
   srand(time(NULL));
   float bono;
   int rdm1, rdm2;
-  do
+  cout << "Ingrese la cantidad del bono: ";
+  cin >> bono;
+  while (cin.fail() || bono < 0)
   {
+    cout << "\nIngrese un valor valido\n";
+    cin.clear();
+    cin.ignore(256, '\n');
     cout << "Ingrese la cantidad del bono: ";
     cin >> bono;
-    if (bono < 0)
-      cout << "ingrese un valor valido\n";
-  } while (bono < 0);
+  }
   rdm1 = (rand() % 4) + 1;
   rdm2 = rand() % 8;
   employees[rdm1][rdm2].setBono(bono);
   cout << "\nel empleado no. " << employees[rdm1][rdm2].getId() << " es el ganador del bono\n";
   cout << "Su quincena actual es de " << fixed << setprecision(2) << employees[rdm1][rdm2].getQuincena() + employees[rdm1][rdm2].getBono() << " Lempiras sin deducciones\n";
+  system("pause>nul");
 }
 
 bool findEmp(int id)
@@ -472,4 +484,36 @@ bool findEmp(int id)
         state = true;
       }
   return state;
+}
+
+bool validId(int id)
+{
+  if (cin.fail())
+  {
+    cin.clear();
+    cin.ignore(256, '\n');
+    return false;
+  }
+  else
+  {
+    cin.clear();
+    cin.ignore(256, '\n');
+    return true;
+  }
+}
+
+int validOp(int opcion)
+{
+  if (cin.fail())
+  {
+    cin.clear();
+    cin.ignore(256, '\n');
+    return 0;
+  }
+  else
+  {
+    cin.clear();
+    cin.ignore(256, '\n');
+    return opcion;
+  }
 }
